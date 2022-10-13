63ec38b9d9a05663dbe2402a7a16dd61059da70a
Yamu na <yamuna.jayaraman@gmail.com>
2022-10-13 17:08:51 +0530

    Remove element from an aray

--- src/test/java/arrayProblems/RemoveAllOccurancesOfTargetElement.java
+++ src/test/java/arrayProblems/RemoveAllOccurancesOfTargetElement.java
@@ -2,7 +2,9 @@ package arrayProblems;
 
 import org.testng.Assert;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class RemoveAllOccurancesOfTargetElement {
     //Problem statement: Given an integer array and a target value k, remove all the occurrences of the target
@@ -15,7 +17,10 @@ public class RemoveAllOccurancesOfTargetElement {
         int[] output = remove(a, 5);
         Assert.assertEquals(new int[]{1,2,3,4,6},output);
                 System.out.println("output : " + Arrays.toString(output));
-            }
+
+        System.out.println("output : " + Arrays.toString(removeUsingStream(a,5)));
+        System.out.println("output : " + Arrays.toString(removeUsingArrayList(a,5)));
+    }
 
     public static int[] remove(int[] input, int element) {
         int count = 0;
@@ -37,5 +42,26 @@ public class RemoveAllOccurancesOfTargetElement {
         }
         input=null;
         return result;
+
     }
+
+    //Using Stream
+    public static int[] removeUsingStream(int[] input, int element){
+        return Arrays.stream(input).filter(value -> value!=element).toArray();
+    }
+
+    //Using ArrayList
+    public static int[] removeUsingArrayList(int[] input, int element) {
+    List<Integer> result = new ArrayList<Integer>();
+
+    for(int i:input){
+        if(i!=element)
+            result.add(i);
+        }
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+
+
+
 }
